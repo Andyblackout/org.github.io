7fbe7134be9a5be1e2407cb8afd8dd1c6bfcaf1f
Alvaro Nieves <109119092+Andyblackout@users.noreply.github.com>
2023-07-03 16:28:11 +0000

    solucionando errores de las imagenes

---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,24 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer
+      className="footer"
+      style={{ backgroundImage: "url(./img/Footer.png)" }}
+    >
+      <div className="redes">
+        <a href="https://www.facebook.com/Alvblackout/">
+          <img src="/img/facebook.png" alt="Facebook" />
+        </a>
+        <a href="https://twitter.com/AlvBlackout">
+          <img src="/img/twitter.png" alt="Twitter" />
+        </a>
+        <a href="https://www.instagram.com/andyblackout/">
+          <img src="/img/instagram.png" alt="instagram" />
+        </a>
+      </div>
+      <img src="/img/Logo.png" alt="org" />
+      <strong>Desarrollado por Alvaro Nieves</strong>
+    </footer>
+  );
+};
+export default Footer;
